c9648aad6f02851461cc10176f4731f6124ec552
pavan.k <pavan.k@digitalapicraft.com>
2020-05-09 12:34:02 +0530

    HYPN - types - added UpdatePlantDTO

--- types/plant.go
+++ types/plant.go
@@ -1,16 +1,16 @@
 package types
 
+// PlantDTO creates a plant
 type PlantDTO struct {
 	Name           string `json:"name" binding:"required"`
 	NumberOfPlants int    `json:"numberOfPlants" binding:"required"`
 	CategoryID     uint   `json:"categoryID" binding:"required"`
 }
 
-type GrowthDTO struct {
-	PlantID   int    `json:"plantID" binding:"required"`
-	AirTemp   string `json:"airTemp" binding:"required"`
-	WaterTemp string `json:"waterTemp" binding:"required"`
-	Humidity  string `json:"humidity" binding:"required"`
-	Color     string `json:"color" binding:"required"`
-	Lighting  string `json:"lighting" binding:"required"`
+// UpdatePlantDTO updates a plant
+// TODO add more fields
+type UpdatePlantDTO struct {
+	Name           string `json:"name"`
+	NumberOfPlants int    `json:"numberOfPlants"`
+	CategoryID     uint   `json:"categoryID"`
 }
